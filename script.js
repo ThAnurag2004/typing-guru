d0f0a591e4eff942b02578d1884730248c76b853
Anurag Singh <abc103498@gmail.com>
2025-06-29 20:59:02 +0530

    stage3.2

--- script.js
+++ script.js
@@ -6,6 +6,24 @@ let startTime;
 let endTime;
 let typingStarted = false;
 
+
+window.addEventListener('keydown', (e) => {
+    const key = e.code;
+    const button = document.getElementById(key);
+    if (button) {
+        button.style.backgroundColor = "yellow";
+    }
+});
+
+window.addEventListener('keyup', (e) => {
+    const key = e.code;
+    const button = document.getElementById(key);
+    if (button) {
+        button.style.backgroundColor = "white";
+    }
+});
+
+
 // Wrap each character in span
 para.innerHTML = text
   .split('')
